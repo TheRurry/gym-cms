3d7a8ed12c0fdc374647811ee6d218734370dbd2
Sam Pham <sam.h.pham@gmail.com>
2018-03-10 23:49:30 +0000

    Added some map boundaries

--- gym_cms/envs/cms_env.py
+++ gym_cms/envs/cms_env.py
@@ -10,8 +10,8 @@ class CmsEnv(gym.Env):
         self.mshards = []
         self.step = 0
         self.reset()
-    
-    def reset(self):  
+
+    def reset(self):
         while len(mshards) < 20:
 	    new_pos = [rnd.randint(0,63), rnd.randint(0,63)]
             if new_pos not in mshards:
@@ -19,19 +19,20 @@ class CmsEnv(gym.Env):
         marines = np.random.rand(64, size = (20, 2))
         step = 0
         return (self.marines, self.mshards)
-    
+
     def step(self, action):
         reward = 0
-      
+
         for marine in marines:
             if marine in mshard:
                 mshard.remove(marine)
                 reward += 1
-          
+
         action = [-1,-1]
         actions[0] = action // 4
         actions[1] = action % 4
-      
+
+        # Apply action
         for x in range(0,2):
             if actions[x] == 0: # UP
                 marines[x][1] -= 1
@@ -41,11 +42,19 @@ class CmsEnv(gym.Env):
                 marines[x][0] -= 1
             elif actions[x] == 3: # RIGHT
                 marines[x][0] += 1
-          
+
+        # Check to see if marine has gone out of the map. Assume map is square
+        for x in range(0,2):
+            for y in range(0,2):
+                if marines[x][y] >= 64:
+                    marines[x][y] = 63
+                elif marines[x][y] < 0:
+                    marines[x][y] = 0
+
         step += 1
         done = False
         if step == 240:
             done = True
-      
+
         state = (self.marines, self.mshards)
-        return state, reward, done, {} 
+        return state, reward, done, {}
