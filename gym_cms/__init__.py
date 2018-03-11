03140a60aac282dd7685d1505d3a26308dca27e1
Ryan <zcabrdc@ucl.ac.uk>
2018-03-11 14:55:35 +0000

    Added hottercolder environment.

--- gym_cms/__init__.py
+++ gym_cms/__init__.py
@@ -4,3 +4,8 @@ register(
     id='cms-v0',
     entry_point='gym_cms.envs:CmsEnv',
 )
+
+register(
+    id='hot-v0',
+    entry_point='gym_cms.envs:HotterColder',
+)
--- gym_cms/envs/__init__.py
+++ gym_cms/envs/__init__.py
@@ -1,1 +1,2 @@
 from gym_cms.envs.cms_env import CmsEnv
+from gym_cms.envs.hotter_colder import HotterColder
