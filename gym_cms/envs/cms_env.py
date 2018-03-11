0384bf34f050f2669827e99b0fc9e0bc39b390c5
Sam Pham <sam.h.pham@gmail.com>
2018-03-11 17:35:45 +0000

    BLah

--- gym_cms/envs/cms_env.py
+++ gym_cms/envs/cms_env.py
@@ -23,10 +23,16 @@ class CmsEnv(gym.Env):
         self.marines = []
         self.mshards = []
 
-        while len(self.mshards) < 1:
-            new_pos = [rnd.randint(0,HEIGHT - 1), rnd.randint(0,HEIGHT - 1)]
-            if new_pos not in self.mshards:
-                self.mshards.append(new_pos)
+        # while len(self.mshards) < 1:
+        #     new_pos = [rnd.randint(0,HEIGHT - 1), rnd.randint(0,HEIGHT - 1)]
+        #     if new_pos not in self.mshards:
+        #         self.mshards.append(new_pos)
+
+        new_pos = [2, 2]
+        self.mshards.append(new_pos)
+        new_pos = [4, 4]
+        self.mshards.append(new_pos)
+
         self.marines.append([rnd.randint(0,WIDTH - 1), rnd.randint(0,HEIGHT - 1)])
         # self.marines.append([rnd.randint(0,WIDTH - 1), rnd.randint(0,HEIGHT - 1)])
         self.steps = 0
@@ -38,6 +44,8 @@ class CmsEnv(gym.Env):
             observation[shard[0] + HEIGHT * shard[1]] = MINERAL
         for marine in self.marines:
             observation[marine[0] + HEIGHT * marine[1]] = MARINE
+        # for x in range(5):
+        #     print(observation[5 * x], "" ,  observation[5 * x + 1], "" ,observation[5 * x + 2], "" ,observation[5 * x + 3], "" ,observation[5 * x + 4])
         return np.array(observation)
 
     def step(self, action):
@@ -75,20 +83,30 @@ class CmsEnv(gym.Env):
             self.marines[0][0] += 1
 
         # Check to see if marine has gone out of the map. Assume map is square
+        boundary = False
         if self.marines[0][1] >= HEIGHT:
+            boundary = True
             self.marines[0][1] = HEIGHT - 1
         elif self.marines[0][1] < 0:
+            boundary = True
             self.marines[0][1] = 0
         if self.marines[0][0] >= WIDTH:
+            boundary = True
             self.marines[0][0] = WIDTH - 1
         elif self.marines[0][0] < 0:
+            boundary = True
             self.marines[0][0] = 0
+
+        # if boundary:
+        #     reward = -1
 
         self.steps += 1
         done = False
         if self.steps == 10 or len(self.mshards) == 0:
             done = True
+            print("Steps taken:", self.steps)
             print("Rem Shards:", len(self.mshards))
 
         state = self.observation()
+        # print(reward)
         return state, reward, done, {}
